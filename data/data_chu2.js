2f764c258ad00325b67c9c298ea3593b219bd9b1
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-07-31 22:57:55 +0900

    Create data_chu2.js

---
+++ data/data_chu2.js
@@ -0,0 +1,10 @@
+const CURRENT_QUIZ_DATA = {
+    title: "📐 中学2年生 数学（記述式証明）",
+    questions: [
+        {
+            type: "記述",
+            q: "【問題】AB = AC の二等辺三角形ABCにおいて、辺BCの中点をMとするとき、△ABM ≡ △ACM であることを証明しなさい。",
+            modelAnswer: "【模範解答】\n△ABMと△ACMにおいて、\n仮定より、AB = AC ーー①\nMはBCの中点なので、BM = CM ーー②\n共通な辺なので、AM = AM ーー③\n①、②、③より、3組の辺がそれぞれ等しいので、\n△ABM ≡ △ACM である。"
+        }
+    ]
+};
