781bf6c7d1adba9ee1a861de32bb89da211e3df0
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-07-31 22:56:22 +0900

    Create data_shou5.js

---
+++ data/data_shou5.js
@@ -0,0 +1,7 @@
+const CURRENT_QUIZ_DATA = {
+    title: "🎒 小学5年生 算数（無限ランダム）",
+    questions: [
+        { type: "基礎", isRandom: true, pattern: "shou5_mul" },
+        { type: "基礎", isRandom: true, pattern: "shou5_mul" }
+    ]
+};
