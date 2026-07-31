28d02fa356982b3970273046319292252130ed16
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-07-31 22:54:30 +0900

    Create data_shou3.js

---
+++ data/data_shou3.js
@@ -0,0 +1,8 @@
+const CURRENT_QUIZ_DATA = {
+    title: "🎒 小学3年生 算数（無限ランダム）",
+    questions: [
+        { type: "基礎", isRandom: true, pattern: "shou3_div" },
+        { type: "基礎", isRandom: true, pattern: "shou3_div" },
+        { type: "基礎", isRandom: true, pattern: "shou3_div" }
+    ]
+};
