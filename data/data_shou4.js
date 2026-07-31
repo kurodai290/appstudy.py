6e1b32a7183654526f264e55983d68ff57560729
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-07-31 22:55:59 +0900

    Create data_shou4.js

---
+++ data/data_shou4.js
@@ -0,0 +1,7 @@
+const CURRENT_QUIZ_DATA = {
+    title: "🎒 小学4年生 算数（無限ランダム）",
+    questions: [
+        { type: "基礎", isRandom: true, pattern: "shou4_dec" },
+        { type: "基礎", isRandom: true, pattern: "shou4_dec" }
+    ]
+};
