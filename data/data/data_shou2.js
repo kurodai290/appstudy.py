3905e19b453af1ccd6b12abb13f150e5c34558cd
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-07-31 22:53:33 +0900

    Create data_shou2.js

---
+++ data/data/data_shou2.js
@@ -0,0 +1,8 @@
+const CURRENT_QUIZ_DATA = {
+    title: "🎒 小学2年生 算数（無限ランダム）",
+    questions: [
+        { type: "基礎", isRandom: true, pattern: "shou2_kuku" },
+        { type: "基礎", isRandom: true, pattern: "shou2_kuku" },
+        { type: "基礎", isRandom: true, pattern: "shou2_kuku" }
+    ]
+};
