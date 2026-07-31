255bb6a5a80b8b27c0578d0aafbb4fe6e1ce34ff
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-07-31 22:56:49 +0900

    Create data_shou6.js

---
+++ data/data_shou6.js
@@ -0,0 +1,7 @@
+const CURRENT_QUIZ_DATA = {
+    title: "🎒 小学6年生 算数（無限ランダム）",
+    questions: [
+        { type: "基礎", isRandom: true, pattern: "shou6_ratio" },
+        { type: "基礎", isRandom: true, pattern: "shou6_ratio" }
+    ]
+};
