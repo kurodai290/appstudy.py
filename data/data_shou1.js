080a5432d9fe846ad6537e398642574283ac38f1
kurodai290 <riku.natsu.kurodai@gmail.com>
2026-07-31 22:53:04 +0900

    Create data_shou1.js

---
+++ data/data_shou1.js
@@ -0,0 +1,9 @@
+const CURRENT_QUIZ_DATA = {
+    title: "🎒 小学1年生 算数（無限ランダム）",
+    questions: [
+        { type: "基礎", isRandom: true, pattern: "shou1_add" },
+        { type: "基礎", isRandom: true, pattern: "shou1_add" },
+        { type: "基礎", isRandom: true, pattern: "shou1_sub" },
+        { type: "基礎", isRandom: true, pattern: "shou1_sub" }
+    ]
+};
